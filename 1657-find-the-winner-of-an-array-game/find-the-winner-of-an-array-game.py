7b169359ce75d7462a374479e3f771a0845333fb
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2023-11-04 20:36:27 -0700

    Time: 524 ms (71.20%) | Memory: 29.6 MB (74.46%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.py
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.py
@@ -2,20 +2,21 @@ class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
        
         left = arr[0]
-        flips = 0
+        winstreak = 0
         for r in range(1, len(arr)):
             right = arr[r]
             if right > left:
                 arr[0], arr[r] = arr[r], arr[0]
             
             if left == arr[0]:
-                flips += 1
+                winstreak += 1
             else:
-                flips = 1
+                winstreak = 1
                 left = arr[0]
             
-            if flips == k:
+            if winstreak == k:
                 return arr[0]
         
+        # when k > len(nums), we take the largest in the arr
         return arr[0]
         

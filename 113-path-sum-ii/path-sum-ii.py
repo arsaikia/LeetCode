292d5769a8db439e8e06daadae668dededabfd7b
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2023-11-08 23:46:01 -0800

    Time: 60 ms (6.80%) | Memory: 17.8 MB (82.11%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        paths = []
+        self.getPaths(root, [], paths, targetSum)
+        return paths
+    
+    def getPaths(self, node, currPath, paths, targetSum):
+        if not node:
+            return False
+        
+        currPath.append(node.val)
+
+        if not node.left and not node.right and targetSum == node.val:
+            paths.append(currPath[:])
+            currPath.pop()
+            return
+            
+        self.getPaths(node.left, currPath, paths, targetSum - node.val)
+        self.getPaths(node.right, currPath, paths, targetSum - node.val)
+        
+        currPath.pop()
+        
+        
+
+        

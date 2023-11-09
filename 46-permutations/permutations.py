8e1b75aa6fa5ad269d2304ab5e171a4507142e37
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2023-11-09 00:29:34 -0800

    Time: 44 ms (62.56%) | Memory: 16.8 MB (15.94%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,16 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(curr):
-            if len(curr) == len(nums):
-                ans.append(curr[:])
-                return
-        
-            for num in nums:
-                if num not in curr:
-                    curr.append(num)
-                    backtrack(curr)
-                    curr.pop()
-            
-        ans = []
-        backtrack([])
-        return ans
+        res = []
+        q = collections.deque(nums)
+
+        # base case
+        if len(nums) == 1:
+            return [nums.copy()]  # q[:] is a deep copy
+
+        for i in range(len(q)):
+            n = q.popleft()
+            perms = self.permute(q)
+
+            for perm in perms:
+                perm.append(n)
+            res.extend(perms)
+            q.append(n)
+        return res

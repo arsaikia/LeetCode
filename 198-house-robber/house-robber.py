96d868b05be2981305e464b59baceac2b80c94cb
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2023-11-10 01:58:39 -0800

    Time: 42 ms (37.12%) | Memory: 16.4 MB (13.60%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -3,13 +3,11 @@ class Solution:
         secondLast = 0
         last = 0
 
-        # [2, 3, 1, 4]
         for i in range(len(nums)):
             tmp = max(nums[i] + secondLast, last)
             secondLast = last
             last = tmp
 
-        
         return last
 
         

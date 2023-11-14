aee7f3046f319794c57b61cf90dd9e9ef197ea1b
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2023-11-13 22:15:27 -0800

    Time: 38 ms (70.89%) | Memory: 16.3 MB (61.64%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -10,21 +10,19 @@ class Solution:
                 return True
             return False
     
-    def traverse(self, s, i):
-            if i in self.ways:
-                return self.ways[i]
-            if s[i] == "0":
-                return 0
-            
-            res = self.traverse(s, i + 1)
-
-            if self.canHaveValidDoubleDigitsChar(i, s):
-                res += self.traverse(s, i + 2)
-            
-            self.ways[i] = res
-            return self.ways[i]
-    
     def numDecodings(self, s: str) -> int:
-        self.ways = { len(s) : 1 }        
-        return self.traverse(s, 0)
+        self.ways = { len(s) : 1 }   
+        
+        for idx in reversed(range(len(s))):
+            if s[idx] == "0":
+                self.ways[idx] = 0
+            else:
+                self.ways[idx] = self.ways[idx + 1]
+
+                if self.canHaveValidDoubleDigitsChar(idx, s):
+                    self.ways[idx] += self.ways[idx + 2]
+        
+        return self.ways[0]
+            
+
         

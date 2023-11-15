96d33e24972d883f371bc4aca92dfc5bb9fd0f79
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2023-11-14 22:55:18 -0800

    Time: 53 ms (38.43%) | Memory: 20.1 MB (35.92%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = {} # (idx, isBuying)
+
+        def getProfit(idx, isBuying):
+
+            if idx >= len(prices):
+                return 0
+            
+            if (idx, isBuying) in dp:
+                return dp[(idx, isBuying)]
+            
+            if isBuying:
+                buy = getProfit(idx + 1, not isBuying) - prices[idx]
+                cooldown = getProfit(idx + 1, isBuying)
+                dp[(idx, isBuying)] = max(buy, cooldown)
+            else:
+                sell = getProfit(idx + 2, not isBuying) + prices[idx]
+                cooldown = getProfit(idx + 1, isBuying)
+                dp[(idx, isBuying)] = max(sell, cooldown)
+            
+            return dp[(idx, isBuying)]
+        return getProfit(0, True)

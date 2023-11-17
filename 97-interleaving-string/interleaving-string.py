d509c1ca23a069cb5826c7ef02c6c2069363ce68
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2023-11-17 00:07:13 -0800

    Time: 65 ms (7.05%) | Memory: 16.5 MB (50.88%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -1,23 +1,16 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        cache = {}
-        
-        def topDownDP(i, j):
-
-            if i == len(s1) and j == len(s2) and i + j == len(s3):
-                return True
-            
-            if (i, j) in cache:
-                return cache[(i, j)]
-
-            if i < len(s1) and i + j < len(s3) and s1[i] == s3[i + j] and topDownDP(i + 1, j):
-                return True
-
-            if j < len(s2) and i + j < len(s3) and s2[j] == s3[i + j] and topDownDP(i, j + 1):
-                return True
-            
-            cache[(i, j)] = False
-            
+        if len(s1) + len(s2) != len(s3):
             return False
-            
-        return topDownDP(0, 0)
+
+        dp = [ [False for __ in range(len(s2) + 1)] for __ in range(len(s1) + 1) ]
+        dp[-1][-1] = True
+
+        for i in reversed(range(len(s1) + 1)):
+            for j in reversed(range(len(s2) + 1)):
+                if i < len(s1) and s1[i] == s3[i + j] and dp[i + 1][j]:
+                    dp[i][j] = True
+                if j < len(s2) and s2[j] == s3[i + j] and dp[i][j + 1]:
+                    dp[i][j] = True
+        
+        return dp[0][0]

d8e75960f22072d7eda2e2a31357d6ce9288e470
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2023-11-23 13:59:02 -0800

    Time: 105 ms (74.22%) | Memory: 17.1 MB (31.63%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s) or t == "":
+            return ""
+        
+        charsInS, charsInT = collections.defaultdict(int), collections.defaultdict(int)
+        for ch in t:
+            charsInT[ch] += 1
+
+        have, need = 0, len(charsInT)
+        res, size = [-1, -1], float("inf")
+        l = 0
+
+        
+
+        for r in range(len(s)):
+            if have == need and r - l + 1 < size:
+                res = [l, r]
+            
+            # open window
+            if s[r] in charsInT:
+                charsInS[s[r]] += 1
+                if charsInS[s[r]] == charsInT[s[r]]:
+                    have += 1
+            
+            # close window
+            while have == need:
+                if r - l + 1 < size:
+                    size = r - l + 1
+                    res = [l, r]
+                
+                # close from l
+                if s[l] in charsInT:
+                    charsInS[s[l]] -= 1
+                    if charsInS[s[l]] + 1 == charsInT[s[l]]:
+                        have -= 1
+                l += 1
+        l, r = res
+        return s[l : r + 1] if size != float("inf") else ""
+
+        
+
+        

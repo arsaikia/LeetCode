daee50e8539366edff697028d999faa53ddf883d
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-03-31 23:11:27 -0700

    Time: 166 ms (41.61%) | Memory: 17.9 MB (97.60%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,9 +1,23 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counts = collections.Counter(nums)
-        maj = len(nums) // 2
+        # Boyer-Moore algorithm
+        # Very Very Important
 
-        for key, val in counts.items():
-            if val > maj:
-                return key
+        candidate = nums[0]
+        votes = 0
+
+        for num in nums:
+            if num == candidate:
+                votes += 1
+            elif votes > 0:
+                votes -=1
+            else:
+                candidate = num
+                votes = 1
+        
+        return candidate
+
+
+            
+            
         

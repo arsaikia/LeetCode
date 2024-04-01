bcf96ea2c251f1a857796d58761f300321cdbed2
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-03-31 20:26:21 -0700

    Time: 29 ms (88.59%) | Memory: 16.6 MB (82.28%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -1,20 +1,11 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         length = 0
-
         i = len(s) - 1
-
-        # move right pointer to left as long as we have empty string
-        # stops when we find a valid character
-        while i >= 0 and s[i] == " ":
+        while i >= 0 and s[i] == ' ':
             i -= 1
-        
-        # Now move i to left as long as char is not empty string
-        while i >= 0 and s[i] != " ":
-            i -= 1
+        while i >= 0 and s[i] != ' ':
             length += 1
-        
+            i -= 1
         return length
-        
-
         

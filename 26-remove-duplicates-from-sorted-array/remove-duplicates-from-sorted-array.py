394755d885cf90bc4f29292068346eecab72c281
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-04-01 20:37:31 -0700

    Time: 86 ms (21.91%) | Memory: 18 MB (58.39%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,11 +1,18 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        # Approach:
+        # Similar to move all 0's to the end of array,
+        # In this problem, rather than removing duplicates, we move all unique elements to
+        # the begining of the list
 
-        lastUniqueIdx = 1
+        # List with 1 element will always be unique
+        # Set duplicate is found at idx = 1
+        duplicateElementIdx = 1
         for i in range(1, len(nums)):
             # found a unique pair
+            # For every unique pair, move the unique element to the last duplicate element idx
             if nums[i - 1] != nums[i]:
-                nums[lastUniqueIdx] = nums[i]
-                lastUniqueIdx += 1
+                nums[duplicateElementIdx] = nums[i]
+                duplicateElementIdx += 1
         
-        return lastUniqueIdx
+        return duplicateElementIdx

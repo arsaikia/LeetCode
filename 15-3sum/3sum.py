ee23ddec0a811b998328b5131feb48b4a3dd4131
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-04-01 21:08:52 -0700

    Time: 2613 ms (5.00%) | Memory: 20 MB (91.70%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,20 +3,20 @@ class Solution:
         nums.sort()
         triplets = set()
 
-        for idx in range(len(nums)):
-            val = nums[idx]
+        for idx, num in enumerate(nums):
             l, r = idx + 1, len(nums) - 1
             while l < r:
-                small, large = nums[l], nums[r]
-                if val + small + large == 0:
-                    triplets.add((val, small, large))
-                        
+                left, right = nums[l], nums[r]
+                val = num + left + right
+                if val > 0:
+                    r -= 1
+                elif val < 0:
                     l += 1
-                    r -= 1
-                elif val + small + large > 0:
-                    r -= 1
                 else:
+                    triplet = tuple(sorted([num, left, right]))
+                    triplets.add(triplet)
                     l += 1
-        return triplets
-
-
+                    r -= 1
+        
+        return list(triplets)
+        

86eb149f76fec991374b51c27d3c1efac127cdb4
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-04-02 22:05:59 -0700

    Time: 7709 ms (5.26%) | Memory: 16.6 MB (71.59%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -2,51 +2,62 @@ class Trie:
     def __init__(self, word):
         self.root = {}
         self.endSymbol = "*"
-
         self.insert(word)
-        
+
     def insert(self, word):
         node = self.root
         for ch in word:
             if ch not in node:
                 node[ch] = {}
             node = node[ch]
-        node[self.endSymbol] = word
-
+        node[self.endSymbol] = True
 
 class Solution:
+        
+    def backtrack(self, row, col, grid, visited, node):
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        if (row not in range(ROWS) or col not in range(COLS) or (row, col) in visited):
+            return False
+
+        char = grid[row][col]
+
+        if char not in node:
+            return False
+
+        visited.add((row, col))
+        node = node[char]
+
+        if "*" in node:
+            return True
+        
+        found = (
+            self.backtrack(row + 1, col, grid, visited, node) or
+            self.backtrack(row - 1, col, grid, visited, node) or
+            self.backtrack(row, col + 1, grid, visited, node) or
+            self.backtrack(row, col - 1, grid, visited, node)
+        )
+
+        if found:
+            return True
+
+        visited.remove((row, col))
+        return False
+
     def exist(self, board: List[List[str]], word: str) -> bool:
         trie = Trie(word)
-        ROWS, COLS = len(board), len(board[0])
-        res, visited = set(), set()
-
-        def dfs(row, col, node):
-            if (
-                row not in range(ROWS) or 
-                col not in range(COLS) or
-                (row, col) in visited  or
-                board[row][col] not in node
-                ):
-                return
-            
-            visited.add((row, col))
-            node = node[board[row][col]]
-
-            if "*" in node:
-                res.add(node["*"])
-
-            dfs(row + 1, col, node)
-            dfs(row, col + 1, node)
-            dfs(row - 1, col, node)
-            dfs(row, col - 1, node)
-
-            visited.remove((row, col))
-        
+        root = trie.root
+        ROWS = len(board)
+        COLS = len(board[0])
+        visited = set()
 
         for row in range(ROWS):
             for col in range(COLS):
-                dfs(row, col, trie.root)
+                wordExists = self.backtrack(row, col, board, visited, root)
+                if wordExists:
+                    return True
         
-        return list(res)
-
+        return False
+        
         

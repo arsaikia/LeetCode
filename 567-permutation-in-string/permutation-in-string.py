77a3a65e71ed130a5a7bd42a5cdf1c44b6ea2d0e
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-04-15 23:24:23 -0700

    Time: 64 ms (55.77%) | Memory: 16.7 MB (79.10%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,28 +1,26 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        
         if len(s1) > len(s2):
             return False
         
         s1Chars = [0] * 26
-        s1Set = set(s1)
-        for ch in s1:
-            s1Chars[ord(ch) - ord("a")] += 1
+        for char in s1:
+            s1Chars[ord(char) - ord("a")] += 1
         
-        l = 0
         s2Chars = [0] * 26
+        l = 0
+
         for r in range(len(s2)):
-            if s2[r] not in s1Set:
-                continue
-            
             s2Chars[ord(s2[r]) - ord("a")] += 1
 
-            while r - l + 1 > len(s1):
-                if s2[l] in s1Set:
-                    s2Chars[ord(s2[l]) - ord("a")] -= 1
+            # close window if in window chars > len(s1)
+            while (r - l + 1) > len(s1):
+                s2Chars[ord(s2[l]) - ord("a")] -= 1
                 l += 1
             
-            if r - l + 1 == len(s1) and s1Chars == s2Chars:
+            if (r - l + 1) == len(s1) and s1Chars == s2Chars:
                 return True
         
         return False
-
+        

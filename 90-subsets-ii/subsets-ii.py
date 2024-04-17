a77261328dfffa92814811e34dfe1ca9224ec1dd
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-04-16 23:53:50 -0700

    Time: 40 ms (54.07%) | Memory: 16.8 MB (50.44%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,24 +1,25 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-
         subsets = []
 
         def backtrack(idx, subset):
             if idx == len(nums):
                 subsets.append(subset[:])
-                return 
+                return
             
-            # consider curr elements
+            # add current element to subset
             subset.append(nums[idx])
             backtrack(idx + 1, subset)
 
-            # do not consider any elements same as current
-            subset.pop()
-            while idx + 1 < len(nums) and nums[idx] == nums[idx + 1]:
+            # do not include current to subset
+            # HERE - because we have duplicates, the next might be same as previous
+            # So a path that does not contain an element, it also can't contain any of its possible duplicates
+            while idx < len(nums) - 1 and nums[idx] == nums[idx + 1]:
                 idx += 1
+            subset.pop()
             backtrack(idx + 1, subset)
-        
+
         backtrack(0, [])
         return subsets
-        
+

2c7be79c923aadcebd630478ab93c527a7b678f7
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-04-17 00:38:47 -0700

    Time: 47 ms (83.11%) | Memory: 16.7 MB (43.60%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort(reverse = True)
         combinations = []
-
 
         def backtrack(idx, combination, total):
             if total == target:

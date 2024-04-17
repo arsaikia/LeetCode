bcaf43f898c439904cb5d0b6e77cb19f788c906a
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-04-16 22:26:31 -0700

    Time: 39 ms (82.16%) | Memory: 16.5 MB (94.15%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
@@ -3,15 +3,17 @@ class Solution:
         inWindow = collections.defaultdict(int)
 
         l = 0
-        res = 0
+        longestSubstring = 0
 
         for r in range(len(s)):
+            # add current char to window
             inWindow[s[r]] += 1
 
+            # close window from left as long as we have > 2 of current char
             while inWindow[s[r]] > 2:
                 inWindow[s[l]] -= 1
                 l += 1
             
-            res = max(res, r - l + 1)
+            longestSubstring = max(longestSubstring, r - l + 1)
         
-        return res
+        return longestSubstring

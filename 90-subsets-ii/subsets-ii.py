7151fa76517483bf8fcc2bbe841982651eb3c0c3
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-04-17 22:02:31 -0700

    Time: 47 ms (13.53%) | Memory: 16.7 MB (50.51%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -4,22 +4,18 @@ class Solution:
         subsets = []
 
         def backtrack(idx, subset):
-            if idx == len(nums):
+            if idx >= len(nums):
                 subsets.append(subset[:])
                 return
             
-            # add current element to subset
             subset.append(nums[idx])
             backtrack(idx + 1, subset)
 
-            # do not include current to subset
-            # HERE - because we have duplicates, the next might be same as previous
-            # So a path that does not contain an element, it also can't contain any of its possible duplicates
-            while idx < len(nums) - 1 and nums[idx] == nums[idx + 1]:
+            while idx + 1 < len(nums) and nums[idx] == nums[idx + 1]:
                 idx += 1
             subset.pop()
             backtrack(idx + 1, subset)
 
         backtrack(0, [])
         return subsets
-
+        

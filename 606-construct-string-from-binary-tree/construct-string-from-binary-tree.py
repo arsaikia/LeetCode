ee4f52bbebf174dc1ac048ed6c50b148b81a078e
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-04-21 21:55:54 -0700

    Time: 38 ms (92.86%) | Memory: 18.2 MB (10.49%) - LeetSync

--- 606-construct-string-from-binary-tree/construct-string-from-binary-tree.py
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.py
@@ -1,30 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def tree2str(self, root: Optional[TreeNode]) -> str:
-        # Solution with O(n) time and space complexity
         res = []
-        self.dfs(root, res)
-        return "".join(res)
 
-    def dfs(self, t: TreeNode, res: list):
-        # If the current node is None, do nothing and return
-        if t is None:
-            return
-        res.append(str(t.val))
-
-        # If both left and right children are None, return as there are no more branches to explore
-        if t.left is None and t.right is None:
-            return
-        res.append('(')
-
-        # Recursively call the DFS function for the left child
-        self.dfs(t.left, res)
-        res.append(')')
-
-        # If the right child exists, process it
-        if t.right is not None:
-            res.append('(')
-
-            # Recursively call the DFS function for the right child
-            self.dfs(t.right, res)
-            res.append(')')
+        def dfs(node):
+            if not node:
+                return
             
+            res.append("(")
+            res.append(str(node.val))
+
+            if not node.left and node.right:
+                res.append("()")
+
+            dfs(node.left)
+            dfs(node.right)
+            res.append(")")
+
+        dfs(root)
+        return "".join(res[1:-1])
+        

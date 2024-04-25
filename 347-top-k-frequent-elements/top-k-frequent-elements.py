dac58c17f5c1b56887af52c6916d9b2e15494693
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-04-25 00:26:29 -0700

    Time: 104 ms (8.49%) | Memory: 22 MB (14.20%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,27 +1,25 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counts = collections.Counter(nums)
+        buckets = [[] for i in range(len(nums) + 1)]
+
+        numCount = collections.Counter(nums)
+
+        for num, count in numCount.items():
+            buckets[count].append(num)
         
-        count = collections.defaultdict(list)
-
-        for key, val in counts.items():
-            count[val].append(key)
-        
-        sortedCount = []
-        for num, vals in count.items():
-            sortedCount.append((num, vals))
-
-        sortedCount.sort(reverse=True)
-
         res = []
-        for i in range(k):
-            if k == 0:
-                    break
-            currVals = sortedCount[i][1]
-            for val in currVals:
-                if k == 0:
-                    break
-                res.append(val)
-                k -= 1
 
+        for bucket in reversed(buckets):
+
+            if k == 0:
+                break
+            
+            for num in bucket:
+                res.append(num)
+                k -= 1
+        
         return res
+            
+
+
+        

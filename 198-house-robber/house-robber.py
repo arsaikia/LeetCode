43605073911daeb03782fc9120cdafd9ece525a5
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-04-27 13:27:30 -0700

    Time: 30 ms (90.26%) | Memory: 16.1 MB (100.00%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,7 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) <= 2:
-            return max(nums)
+        if len(nums) == 1:
+            return nums[0]
             
         dp = [nums[0]] + [max(nums[0], nums[1])] + [0 for i in range(len(nums) - 2)]
 

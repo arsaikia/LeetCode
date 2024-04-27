969408573ee4ae7c7c423a3569c1e7f77f5a0183
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-04-26 18:16:53 -0700

    Time: 54 ms (99.90%) | Memory: 20.1 MB (42.47%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -3,20 +3,18 @@ class Solution:
         starts = sorted([i for i, j in intervals])
         ends = sorted([j for i, j in intervals])
 
-        count = 0
-        maxCount = 0
+        roomsUsed, maxRoomsUsed = 0, 0
 
         s, e = 0, 0
+
         while s < len(starts):
             if starts[s] < ends[e]:
                 s += 1
-                count += 1
+                roomsUsed += 1
             else:
                 e += 1
-                count -= 1
-            maxCount = max(maxCount, count)
-
-        return maxCount
+                roomsUsed -= 1
             
-
-            
+            maxRoomsUsed = max(maxRoomsUsed, roomsUsed)
+        
+        return maxRoomsUsed

e7259113388b623e0a241d7e850a8e3ccc42ceb6
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-04-30 01:02:20 -0700

    Time: 59 ms (30.21%) | Memory: 19 MB (10.47%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        dp = {}
+        def backtrack(i):
+            if i >= len(cost):
+                return 0
+            
+            if i in dp:
+                return dp[i]
+            
+            dp[i] = cost[i] + min(backtrack(i + 1), backtrack(i + 2))
+            return dp[i]
+        
+        return min(backtrack(0), backtrack(1))
+        

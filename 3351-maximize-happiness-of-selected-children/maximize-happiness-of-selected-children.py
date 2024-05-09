d59d6536593272fd2467a6da1ba9c2582e4950ad
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-05-08 18:07:07 -0700

    Time: 827 ms (32.40%) | Memory: 43.4 MB (60.87%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
@@ -4,22 +4,13 @@ class Solution:
 
         res = 0
         i = 0
-        j = 0
-
-        # if the kth element is < k, take that first or we will get to 0
-        # while happiness[k - 1] < k:
-        #     val = happiness[k - 1]
-        #     res += val
-        #     k -= 1
-        #     j += 1
 
         while i < k:
-            val = happiness[i] + (-1 * (i + j))
-            res += val if val > 0 else 0
-
+            val = happiness[i] - i
             if val <= 0:
                 break
 
+            res += val if val > 0 else 0
             i += 1
         
         return res

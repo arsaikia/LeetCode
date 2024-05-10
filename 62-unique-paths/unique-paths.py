7fe304b43067bd4fe03737ba894e4d4ce1b0cbc4
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-05-10 00:03:48 -0700

    Time: 35 ms (60.49%) | Memory: 17 MB (6.32%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,16 +1,28 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         ROWS, COLS = m, n
-        memo = [ [0 for col in range(n + 1)] for row in range(m + 1) ]
-        memo[m - 1][n - 1] = 1
+        visited = set()
+        cache = {}
 
-        
-        for row in reversed(range(m)):
-            for col in reversed(range(n)):
-                if row == m - 1 and col == n - 1:
-                    continue
-                memo[row][col] = memo[row + 1][col] + memo[row][col + 1]
-        
-        return memo[0][0]
+        def backtrack(row, col):
+            nonlocal ROWS, COLS, visited
 
+            if (row, col) in cache:
+                return cache[(row, col)]
+
+            if row not in range(ROWS) or col not in range(COLS) or (row, col) in visited:
+                return 0
+            
+            if row == ROWS - 1 and col == COLS - 1:
+                return 1
+            
+            visited.add( (row, col) )
+            ways = 0
+            ways += backtrack(row + 1, col) + backtrack(row, col + 1)
+            visited.remove( (row, col) )
+            
+            cache[(row, col)] = ways
+            return cache[(row, col)]
+        
+        return backtrack(0, 0)
         

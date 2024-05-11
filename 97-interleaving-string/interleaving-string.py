79b80283a7b0cb50baa15416bd6bdc55c3641afc
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-05-11 12:08:28 -0700

    Time: 46 ms (36.67%) | Memory: 16.7 MB (54.36%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -3,14 +3,18 @@ class Solution:
         if len(s1) + len(s2) != len(s3):
             return False
 
-        dp = [ [False for __ in range(len(s2) + 1)] for __ in range(len(s1) + 1) ]
+        dp = [ [False for col in range(len(s2) + 1)] for row in range(len(s1) + 1)]
         dp[-1][-1] = True
 
-        for i in reversed(range(len(s1) + 1)):
-            for j in reversed(range(len(s2) + 1)):
+        for i in reversed(range(len(dp))):
+            for j in reversed(range(len(dp[0]))):
                 if i < len(s1) and s1[i] == s3[i + j] and dp[i + 1][j]:
                     dp[i][j] = True
+                
                 if j < len(s2) and s2[j] == s3[i + j] and dp[i][j + 1]:
                     dp[i][j] = True
         
         return dp[0][0]
+
+
+        

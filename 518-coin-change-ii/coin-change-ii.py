3555f7c21ca079c2c7e11c64026ffade56741032
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-05-10 22:26:31 -0700

    Time: 564 ms (19.93%) | Memory: 163.5 MB (16.00%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -2,24 +2,27 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         dp = {}
 
-        @cache
-        def backtrack(idx, total):
+        def backtrack(idx, need):
 
-            if total == amount:
+            if need == 0:
                 return 1
             
-            if idx >= len(coins) or total > amount:
+            if idx >= len(coins) or need < 0:
                 return 0
+            
+            if (idx, need) in dp:
+                return dp[(idx, need)]
             
             ways = 0
 
             # take current coin -> use same coin again
-            ways += backtrack(idx, total + coins[idx])
+            ways += backtrack(idx, need - coins[idx])
 
             # skip curr coin
-            ways += backtrack(idx + 1, total)
+            ways += backtrack(idx + 1, need)
 
+            dp[(idx, need)] = ways
             return ways
         
-        return backtrack(0, 0)
+        return backtrack(0, amount)
         

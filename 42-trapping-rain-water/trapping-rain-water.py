a3e343d7e6b08566ebddf93ecd04e59064216d39
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-05-13 21:07:56 -0700

    Time: 117 ms (18.93%) | Memory: 18.5 MB (72.32%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -6,15 +6,16 @@ class Solution:
 
         while l < r:
             trapHeight = min(lMax, rMax)
-            leftWater = trapHeight - height[l] if (trapHeight - height[l]) > 0 else 0
-            rightWater = trapHeight - height[r] if (trapHeight - height[r]) > 0 else 0
-            rainCollected += leftWater + rightWater
-
             lMax, rMax = max(lMax, height[l]), max(rMax, height[r])
 
             if height[l] < height[r]:
+                leftWater = trapHeight - height[l] if (trapHeight - height[l]) > 0 else 0
+                rainCollected += leftWater
                 l += 1
             else:
+                rightWater = trapHeight - height[r] if (trapHeight - height[r]) > 0 else 0
+                rainCollected += rightWater
                 r -= 1
+            
         
         return rainCollected

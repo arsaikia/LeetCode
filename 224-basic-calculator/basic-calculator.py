e76fdb7351d268d715ac275df3c9011736729b2f
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-05-15 21:53:26 -0700

    Time: 63 ms (67.68%) | Memory: 18 MB (65.08%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,17 +1,14 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        curr = res = 0
-        sign = 1
-        stack = []
-
+        curr, res, sign, stack = 0, 0, 1, []
 
         for ch in s:
             if ch.isdigit():
-                curr = curr * 10 + int(ch)
-            elif ch in ["+", "-"]:
-                res += sign * curr
-                sign = 1 if ch == "+" else -1
+                curr = (10 * curr) + int(ch)
+            elif ch in "+-":
+                res += curr * sign
                 curr = 0
+                sign = 1 if ch == "+" else -1
             elif ch == "(":
                 stack.append(res)
                 stack.append(sign)
@@ -19,11 +16,11 @@ class Solution:
                 sign = 1
             elif ch == ")":
                 res += curr * sign
+                curr = 0
+
                 res *= stack.pop()
                 res += stack.pop()
-                curr = 0
         
         return res + curr * sign
-
 
         

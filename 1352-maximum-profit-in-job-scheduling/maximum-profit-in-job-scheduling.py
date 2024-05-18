16c3b165f34661c6a065505152b7c025ac8f3628
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-05-18 01:21:08 -0700

    Time: 672 ms (58.26%) | Memory: 53.4 MB (26.58%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -1,12 +1,22 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        intervals = sorted(zip(startTime, endTime, profit))
+        dp = {}
+
+        def dfs(i):
+            if i == len(intervals):
+                return 0
+            
+            if i in dp:
+                return dp[i]
+            
+            # skip curr
+            res = dfs(i + 1)
+
+            # take curr
+            j = bisect.bisect(intervals, (intervals[i][1], -1, -1))
+            dp[i] = res = max(res, intervals[i][2] + dfs(j))
+            return res
         
-        dp = [[0,0]]                                            #   e     s     p     dp
-                                                                #  ––-–  ––-–  ––-–  ––––––––––––––––-–
-        f = lambda x: dp[bisect_left(dp,[x+1])-1][1]            #    3     1    20   [[0,0],[3,20]]
-                                                                #    5     2    20   [[0,0],[3,20],[5,20]]
-        for e, s, p in sorted(zip(endTime,startTime, profit)):  #    6     4    70   [[0,0],[3,20],[5,20],[6,90]]
-                                                                #    9     6    60   [[0,0],[3,20],[5,20],[6,90],[9,150]]
-            dp.append([e, max(f(e),p+f(s))])                    #   10     3   100   [[0,0],[3,20],[5,20],[6,90],[9,150],[10,150]]
-                                                                #                                                             |          
-        return dp[-1][1]                                        #                                                           answer
+        return dfs(0)
+        

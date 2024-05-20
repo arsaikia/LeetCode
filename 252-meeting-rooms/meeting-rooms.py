4c20444ea5164a05b87503de64ecf76cc40549c2
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-05-20 15:28:13 -0700

    Time: 77 ms (28.49%) | Memory: 20 MB (17.84%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.py
+++ 252-meeting-rooms/meeting-rooms.py
@@ -1,12 +1,13 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals.sort(key = lambda x: x[0])
-        
+        intervals.sort()
+
         for i in range(1, len(intervals)):
-            first, second = intervals[i - 1], intervals[i]
-            if first[1] > second[0]:
+            one, two = intervals[i - 1], intervals[i]
+
+            # second starts before 1st ends
+            if two[0] < one[1]:
                 return False
-        
         return True
 
 

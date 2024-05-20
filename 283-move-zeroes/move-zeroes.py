29d154b30ce59fda1c502b6d3c2c30889f5cd25d
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-05-20 15:30:51 -0700

    Time: 122 ms (78.24%) | Memory: 18.2 MB (31.75%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,13 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        idx = 0
+
+        nonZeroIdx = 0
 
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[idx] = nums[i]
-                idx += 1
+                nums[nonZeroIdx], nums[i] = nums[i], nums[nonZeroIdx]
+                nonZeroIdx += 1
         
-        for i in range(idx, len(nums)):
-            nums[i] = 0
         

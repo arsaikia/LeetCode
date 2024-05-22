1e0b9550af0c44636d1af7625f2485ddeae33718
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-05-21 21:43:44 -0700

    Time: 153 ms (79.87%) | Memory: 28 MB (68.36%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,24 +1,21 @@
 class Solution:
+    def swap(self, nums, l, r):
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
+        
         k = k % len(nums)
-        l, r = 0, len(nums) - 1
-
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l + 1, r - 1
         
-        l, r = 0, k - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l + 1, r - 1
+        self.swap(nums, 0, len(nums) - 1)
+
+        self.swap(nums, 0, k - 1)
         
-        l, r = k, len(nums) - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l + 1, r - 1
-
-
-
+        self.swap(nums, k, len(nums) - 1)
+        
+        

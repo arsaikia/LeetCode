74f6bbce5652d19271476f933e12d0dd7cc37bad
ARUNABH SAIKIA <arunabh.matrix@gmail.com>
2024-08-21 17:33:14 -0700

    Time: 38 ms (30.88%) | Memory: 16.3 MB (89.59%) - LeetSync

---
+++ 476-number-complement/number-complement.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findComplement(self, num):
+        # bitmask has the same length as num and contains only ones 1...1
+        bitmask = num
+        bitmask |= (bitmask >> 1)
+        bitmask |= (bitmask >> 2)
+        bitmask |= (bitmask >> 4)
+        bitmask |= (bitmask >> 8)
+        bitmask |= (bitmask >> 16)
+        # flip all bits
+        return bitmask ^ num
